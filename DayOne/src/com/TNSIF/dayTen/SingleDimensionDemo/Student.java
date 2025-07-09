4aa85b8d9b63ed5195000ea1326fddb38204d537
mansinirbhavane <mansinirbhavane@gmail.com>
2025-07-09 21:12:39 +0530

    Day10

---
+++ DayOne/src/com/TNSIF/dayTen/SingleDimensionDemo/Student.java
@@ -0,0 +1,33 @@
+package com.TNSIF.dayTen.SingleDimensionDemo;
+
+public class Student {
+	//data members
+		private int rollNo;
+		private String name;
+		
+		//para constructor
+		public Student(int rollNo, String name) {
+			super();
+			this.rollNo = rollNo;
+			this.name = name;
+		}
+
+		public int getRollNo() {
+			return rollNo;
+		}
+
+		public void setRollNo(int rollNo) {
+			this.rollNo = rollNo;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+
+		
+}
