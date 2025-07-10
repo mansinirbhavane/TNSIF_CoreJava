081eac36405e2748d9ad9debe7935c45194db9b5
mansinirbhavane <mansinirbhavane@gmail.com>
2025-07-10 19:47:12 +0530

    Day11

---
+++ DayOne/src/com/TNSIF/dayEleven/FinallyBlock/negativeException.java
@@ -0,0 +1,14 @@
+package com.TNSIF.dayEleven.FinallyBlock;
+
+public class negativeException extends Exception {
+
+	public negativeException(String string)
+	{
+		super (string);
+	}
+	public negativeException()
+	{
+		super ("Percentage should not be negative");
+	}
+	
+}
