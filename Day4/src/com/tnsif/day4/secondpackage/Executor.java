586df47815794f2bfec6e49fe0e9d8fb13cf44b7
mansinirbhavane <mansinirbhavane@gmail.com>
2025-07-18 14:28:40 +0530

    commit

---
+++ Day4/src/com/tnsif/day4/secondpackage/Executor.java
@@ -0,0 +1,20 @@
+package com.tnsif.day4.secondpackage;
+import com.tnsif.day4.firstpackage.Base;
+public class Executor {
+public static void main(String[] args) {
+		
+		
+		//accessing different package class
+		
+		Base b1 = new Base();
+		
+		//private, default , protected and public 
+		
+		b1.methodPublic();
+		b1.varPublic=34567;
+		b1.methodPublic();
+
+		//b1.varDefault=354345;
+	}
+
+}
